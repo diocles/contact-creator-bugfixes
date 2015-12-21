b4849092ba8680ce395e1a4768363f24655bc831
Tim Retout <tim@retout.co.uk>
2015-12-21 21:01:35 +0000

    Implement review feedback: use querySelectorAll()

--- data/fix-menus.js
+++ data/fix-menus.js
@@ -1,9 +1,6 @@
-var classes = ["MainMenuButton", "MainMenuDDButton"];
-for (var i=0; i<classes.length; i++) {
-	var elements = document.getElementsByClassName(classes[i]);
-	for (var j=0; j<elements.length; j++) {
-		if (typeof(elements[j].href) !== 'undefined') {
-			elements[j].href = elements[j].href.replace( /javascript:MenuClick\('(.*)'\)/, "$1");
-		}
+var elements = document.querySelectorAll('.MainMenuButton, .MainMenuDDButton');
+for (var j=0; j<elements.length; j++) {
+	if (typeof(elements[j].href) !== 'undefined') {
+		elements[j].href = elements[j].href.replace( /javascript:MenuClick\('(.*)'\)/, "$1");
 	}
 }
